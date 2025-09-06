274373ccf5258bc9d425e99df8b7ab0a2e5ad751
lucas-cost <lucas_costa@alu.ufc.br>
2025-09-06 13:40:23 -0300

    feat(data-repository): criado camada de contexto e repositorio base

---
+++ src/TimeClockSystem.Infrastructure/Data/Context/TimeClockDbContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TimeClockSystem.Core.Entities;
+
+namespace TimeClockSystem.Infrastructure.Data.Context
+{
+    public class TimeClockDbContext : DbContext
+    {
+        public DbSet<TimeClockRecord> TimeClockRecords { get; set; }
+
+        public TimeClockDbContext(DbContextOptions<TimeClockDbContext> options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<TimeClockRecord>(builder =>
+            {
+                builder.HasKey(r => r.Id);
+                builder.Property(r => r.EmployeeId).IsRequired();
+                builder.Property(r => r.Type).HasConversion<string>();
+                builder.Property(r => r.Status).HasConversion<string>();
+            });
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ src/TimeClockSystem.Infrastructure/Repositories/TimeClockRepository.cs
@@ -0,0 +1,29 @@
+﻿using TimeClockSystem.Core.Entities;
+using TimeClockSystem.Core.Enums;
+using TimeClockSystem.Core.Interfaces;
+
+namespace TimeClockSystem.Infrastructure.Repositories
+{
+    public class TimeClockRepository : ITimeClockRepository
+    {
+        public Task AddAsync(TimeClockRecord record)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<TimeClockRecord?> GetLastRecordForEmployeeAsync(string employeeId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<TimeClockRecord>> GetPendingSyncRecordsAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateStatusAsync(Guid recordId, SyncStatus newStatus)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
