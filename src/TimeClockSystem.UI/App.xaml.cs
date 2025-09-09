51948aa5d92c143bf8a7b1211a061e45c024f928
lucas-cost <lucas_costa@alu.ufc.br>
2025-09-08 21:41:30 -0300

    feat(ui): configurar SQLite DbContext e aplicar migrações na inicialização

--- src/TimeClockSystem.UI/App.xaml.cs
+++ src/TimeClockSystem.UI/App.xaml.cs
@@ -1,13 +1,16 @@
-﻿using Microsoft.Extensions.Configuration;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Polly;
 using System.IO;
 using System.Net.Http;
+using System.Windows;
 using TimeClockSystem.Application.UseCases.RegisterPoint;
 using TimeClockSystem.Core.Interfaces;
 using TimeClockSystem.Core.Settings;
 using TimeClockSystem.Infrastructure.Api;
+using TimeClockSystem.Infrastructure.Data.Context;
 using TimeClockSystem.UI.ViewModels;
 using WpfApplication = System.Windows.Application;
 
@@ -39,6 +42,15 @@ namespace TimeClockSystem.UI
 
             services.Configure<ApiSettings>(configuration.GetSection("ApiSettings"));
 
+            // Database
+            services.AddDbContext<TimeClockDbContext>(options =>
+            {
+                // Certifique-se de que a pasta AppData exista ou escolha outro local
+                string dbPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "TimeClockSystem", "timeclock.db");
+                Directory.CreateDirectory(Path.GetDirectoryName(dbPath)!);
+                options.UseSqlite($"Data Source={dbPath}");
+            });
+
             // Infrastructure
             services.AddAutoMapper(typeof(MappingProfile).Assembly);
             services.AddHttpClient<IApiClient, ApiClient>().AddPolicyHandler(GetRetryPolicy());
@@ -59,11 +71,36 @@ namespace TimeClockSystem.UI
                 .OrResult<HttpResponseMessage>(r => !r.IsSuccessStatusCode)
                 .WaitAndRetryAsync(3, retryAttempt => TimeSpan.FromSeconds(Math.Pow(2, retryAttempt)));
         }
+
+        protected override async void OnStartup(StartupEventArgs e)
+         {
+            try
+            {
+                using (var scope = _host.Services.CreateScope()) 
+                {
+                    TimeClockDbContext dbContext = scope.ServiceProvider.GetRequiredService<TimeClockDbContext>();
+                    await dbContext.Database.MigrateAsync();
+                }
+
+                MainWindow mainWindow = _host.Services.GetRequiredService<MainWindow>();
+                mainWindow.Show();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Ocorreu um erro fatal ao iniciar a aplicação. A aplicação será encerrada.\n\nDetalhes do Erro:\n{ex.ToString()}",
+                                "Erro de Inicialização",
+                                MessageBoxButton.OK,
+                                MessageBoxImage.Error);
+
+                this.Shutdown();
+            }
+        }
+
         protected override async void OnExit(ExitEventArgs e)
         {
             await _host.StopAsync();
             _host.Dispose();
             base.OnExit(e);
+        }
     }
-}
 }
